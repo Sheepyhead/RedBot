06043c84e60afd2b7c709b9b546d6760436754ef
Troels Peter Jessen <kairyuka@live.dk>
2017-09-03 13:34:24 +0200

    Redirected output to file

--- src/main/java/com/sheepy/redbot/Driver.java
+++ src/main/java/com/sheepy/redbot/Driver.java
@@ -2,14 +2,28 @@ package com.sheepy.redbot;
 
 import sx.blah.discord.api.IDiscordClient;
 
+import java.io.*;
+
 
 /**
  * Created by Troels "Sheepyhead" Jessen on 4/28/2017.
  */
 public class Driver {
     public static String BOT_TOKEN = "MzA3NTg4MDM0NzkzMDQ2MDE4.C-XOtg.SmtKgHjBPxzKdzNLB3TF-nJu7e8";
-    public static void main(String[] args) {
+    public static String DEFAULT_OUTPUT_FILE = "output.txt";
 
+    public static void main(String[] args) {
+        try {
+            File outputFile = new File(DEFAULT_OUTPUT_FILE);
+
+            outputFile.delete();
+            outputFile.createNewFile();
+            System.setOut(new PrintStream(new FileOutputStream(DEFAULT_OUTPUT_FILE)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         FeedReader feedReader = new FeedReader(10);
         RedBot bot = new RedBot();
         IDiscordClient client = RedBot.createClient(BOT_TOKEN, true);
